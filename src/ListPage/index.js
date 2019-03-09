beaa15150d9cafa69b911f2b0736811e402f241b
tinyjin <baram991103@gmail.com>
2019-03-09 15:17:39 +0900

    API 연결

--- src/ListPage/index.js
+++ src/ListPage/index.js
@@ -1,2 +1,51 @@
 import '../reset.css';
 import './style.css';
+
+const endPoint = 'https://api-apeast.graphcms.com/v1/cjt0us8481eaa01943b9a2nb4/master';
+const listView = document.querySelector('.list');
+
+window.onload = () => {
+  const query = `
+    {
+      profiles {
+        id
+        name
+        picture {url}
+        introduce
+      }
+    }
+  `;
+
+  fetch(endPoint,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query })
+    })
+    .then(res => res.json())
+    .then((json) => {
+      console.log(json.data.profiles);
+      loadList(json.data.profiles);
+    });
+};
+
+const loadList = (profiles) => {
+  let listHTML = '';
+
+  for (const profile of profiles) {
+    const { name, picture, introduce } = profile;
+    const { url } = picture;
+
+    listHTML += `
+    <li>
+      <img alt="프로필 사진" class="picture" src="${url}">
+      <div class="profile-wrapper">
+        <strong class="name">${name}</strong>
+        <span class="introduce">${introduce}</span>
+      </div>
+    </li>
+    `;
+  }
+
+  listView.innerHTML = listHTML;
+};
