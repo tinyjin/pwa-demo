e556332bd6cdf7ed9f4d896f9881d7395cb2ccd9
tinyjin <baram991103@gmail.com>
2019-03-09 12:03:45 +0900

    프로젝트 생성

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+const meta = {
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  description: 'PWA Demo 2019',
+};
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: dist,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'PWA Demo 2019 - Main Page',
+      filename: 'index.html',
+      template: './src/index.html',
+      meta,
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|gltf|bin|obj|mtl)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            },
+          },
+        ]
+      },
+    ]
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    contentBase: dist,
+  }
+};
